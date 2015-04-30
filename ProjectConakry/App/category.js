37de2b54a87b8a8b10518783df8a96da0cb3e8ed
Kiho <kihoc22@gmail.com>
2015-04-30 00:04:53 -0700

    Fix news section error.

--- ProjectConakry/App/category.js
+++ ProjectConakry/App/category.js
@@ -17,10 +17,11 @@
             return list.splice(1, list.length - 1);
         };
        
-        var groupItems = function (items, count) {
+        var groupItems = function (items, groupSize) {
             var list = [];
             if (items == null)
                 return list;
+            var count = items.length > groupSize ? groupSize : items.length;
             for (var i = 0; i < items.length / count; i++) {
                 list.push({List:[]});
                 for (var j = 0; j < count; j++) {
@@ -36,22 +37,19 @@
             return categoryApi.get(vm.categories)
                 .done(function () {
                     return newsApi.get(vm.news, new Date()).then(
-                    function () {
-                        var groupSize = vm.news().length > 5 ? 5 : vm.news().length;
-                        var list = groupItems(vm.news(), groupSize);
+                    function () {                        
+                        var list = groupItems(vm.news(), 5);
                         vm.newsSections(list);
                     }).then(function () {
                         return eventsApi.get(vm.events, new Date()).then(
                         function () {
-                            var groupSize = vm.events().length > 4 ? 4 : vm.events().length;
-                            var list = groupItems(vm.events(), groupSize);
+                            var list = groupItems(vm.events(), 4);
                             vm.eventsSections(list);
                         });
                     }).then(function () {
                         return loungeApi.get(vm.loungeItems, new Date()).then(
                         function () {
-                            var groupSize = vm.loungeItems().length > 4 ? 4 : vm.loungeItems().length;
-                            var list = groupItems(vm.loungeItems(), groupSize);
+                            var list = groupItems(vm.loungeItems(), 4);
                             vm.loungeSections(list);
                         });
                     });
