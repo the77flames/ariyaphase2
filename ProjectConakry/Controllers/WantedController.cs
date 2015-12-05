da40235684828e41e31d7f722f39c749671e3eb9
theMaestro <theMaestro>
2015-12-05 01:59:37 +0000

    Ariya with Wanted Front End

---
+++ ProjectConakry/Controllers/WantedController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace ProjectConakry.Web.Ariya.Controllers
+{
+    public class WantedController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Subscribe()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjectConakry/js/wanted-main.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $('input[type=range]').mouseup(function () {
+        var range_id = $(this).attr('id');
+        var range_value = $(this).val();
+        $('#range-value-' + range_id).html(range_value);
+    });
+    $('.overlay, .close-modal').click(function () {
+        $('.overlay').hide();
+        $('.modal').hide();
+    });
+});
+var openTerms = function () {
+    $('.overlay').show();
+    $('.modal').show();
+}
